14e303653fad196cca4ae3fba22c3fc63ff85d02
mgim47359-hue <mgim47359@gmail.com>
2026-09-01 08:33:01 +0900

    Add files via upload

---
+++ cineverse-tmdb/config.js
@@ -0,0 +1,5 @@
+// TMDB에서 발급받은 값 중 하나만 입력하세요.
+window.APP_CONFIG = {
+  TMDB_ACCESS_TOKEN: "", // 권장: API Read Access Token (Bearer)
+  TMDB_API_KEY: "b9357db4a6e281da8cd88be064dbdf8a"       // 또는 API Key (v3 auth)
+};
